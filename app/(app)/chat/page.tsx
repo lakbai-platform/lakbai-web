74beae590f9f1126fa237f246daebee6e5db3f37
Johann Buere <johannbuere@gmail.com>
2026-02-26 13:27:06 +0800

    feat: implement chat page displaying chatbox and map area components.

--- app/(app)/chat/page.tsx
+++ app/(app)/chat/page.tsx
@@ -4,13 +4,13 @@ import MapArea from './_components/map-area';
 export default function ChatPage() {
   return (
     <div className='flex h-full w-full overflow-hidden'>
-      {/* LEFT: Chatbox (40%) */}
-      <div className='h-full w-2/5 border-r bg-white'>
+      {/* LEFT: Chatbox (50%) */}
+      <div className='h-full w-1/2 border-r bg-white'>
         <Chatbox />
       </div>
 
-      {/* RIGHT: MapArea (60%) */}
-      <div className='h-full w-3/5 bg-gray-100'>
+      {/* RIGHT: MapArea (50%) */}
+      <div className='h-full w-1/2 bg-gray-100'>
         <MapArea />
       </div>
     </div>
